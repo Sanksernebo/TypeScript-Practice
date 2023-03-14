ede0bf3365af12130485eb0093e25bea07308f11
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-03-14 11:18:05 +0200

    Eesliidese implementeerimine

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -2,8 +2,11 @@ import express, { Express, Request, Response } from "express";
 import stringsController from "./controllers/strings";
 import productController from "./controllers/products";
 import productsController from "./controllers/productlist";
+import cors from "cors";
+
 const app: Express = express();
 
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
