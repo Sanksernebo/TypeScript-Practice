a100845ef8d42c2f381eae2400d253ffc71d92be
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-03-14 10:14:54 +0200

    API päringud mudeliga

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -1,14 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import stringsController from "./controllers/strings";
-
+import productController from "./controllers/products";
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
+app.use("/", productController);
 app.use('/', stringsController);
-
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
 });
